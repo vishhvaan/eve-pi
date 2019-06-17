89db1d09c2185f2c4d624bc437ffcae344b491c8
Vishvaan Gopalakrishnan <gopalav@lri-108141.ccf.org>
2019-06-17 11:05:33 -0400

    Update local repo

--- st_eve.sh
+++ st_eve.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
 
-sudo adduser eve
-sudo adduser eve pi
-sudo adduser eve sudo 
-sudo adduser eve gpio
-sudo adduser eve i2c
+#Add user
+adduser eve
+adduser eve pi
+adduser eve sudo 
+adduser eve gpio
+adduser eve i2c
+passwd eve
 
-# raspi-config to enable i2c and change timezone
-# probably possible to od this automatically
+#Change the timezone
+echo -n "Enter the timezone:"
+read timez
 
-sudo apt update && sudo apt upgrade -y
+echo "Changed."
+echo ""
 
-sudo apt install -y git python3 python3-pip nfs-kernel-server vim tmux libatlas-base-dev
+#Change the hostname
+echo -n "Enter the Hostname (Network Name) of the Device:"
+read hostn
+hostnamectl set-hostname $hostn
+
+echo "Changed."
+echo ""
+
+#Enable I2C
+echo "dtparam=i2c_arm=on" >> /boot/config.txt
+echo "i2c-dev" >> /etc/modules
+
+
+#Install Packages
+apt update && sudo apt upgrade -y
+apt install -y git python3 python3-pip nfs-kernel-server vim tmux libatlas-base-dev
 
 umask 022
-sudo pip3 install adafruit-circuitpython-ads1x15 adafruit-circuitpython-mcp230xx numpy slackclient pandas matplotlib configparser
+pip3 install adafruit-circuitpython-ads1x15 adafruit-circuitpython-mcp230xx numpy slackclient pandas matplotlib configparser
 
-# setting up NFS with EXTREMELY LOOSE SECURITY XD
-echo "/eve   10.0.0.0/8(rw,sync,no_subtree_check)" >> /etc/exports
-sudo systemctl enable nfs-kernel-server.service
-sudo systemctl start nfs-kernel-server.service
+#Git Clone Repo
+mkdir /eve
+git clone https://github.com/vishhvaan/eve-pi.git /eve
+
+#Copy Service to Location
+
 
 echo "//smb-isi1.lerner.ccf.org/scottj10lab/MorbidoData/ /mnt/morbidodata cifs credentials=/home/eve/.smbcredentials,uid=1001,gid=1001 0 0" >> /etc/fstab
 
-## GIT CLONE THE REPO ##
