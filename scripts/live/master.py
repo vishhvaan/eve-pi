7da9daf71f10e1df26bbc7fb4a5764d0130dc282
vishhvaan <gopalav@lri-108141.ccf.org>
2019-07-19 10:24:31 -0400

    Temp Sensing Fixes

--- scripts/live/master.py
+++ scripts/live/master.py
@@ -75,7 +75,7 @@ graph_lock = [0]*totsys
 graph_q = []
 morbidostats = list()
 
-if config['MAIN'].getboolean('temp_sensor'): temp = 0
+if config['MAIN'].getboolean('temp_sensor'): temp = 0.0
 
 def IC_init():
     adc = list()
@@ -222,9 +222,10 @@ def temp_sensor():
             while lines[0].strip()[-3:] != 'YES':
                 time.sleep(0.2)
                 lines = read_temp_raw()
-                equals_pos = lines[1].find('t=')
-                if equals_pos != -1:
+            equals_pos = lines[1].find('t=')
+            if equals_pos != -1:
                     temp_string = lines[1][equals_pos+2:]
+                    global temp
                     temp = float(temp_string) / 1000.0
 
             time.sleep(1)
@@ -735,7 +736,7 @@ class Morbidostat:
                 ODthr.legend(lines, labels, loc=2)
                 # ODplt = (allODs[['current']]).plot()  #figsize=(10,10) in the plot
                 ODfig = ODthr.get_figure()
-                ODfig.savefig("/mnt/morbidodata/%s/%s/ODtemp_%s.png" % (self.sysstr, self.start_time, self.start_time))
+                ODfig.savefig("/mnt/morbidodata/%s/%s/ODtemp_%s.png" % (self.sysstr, self.start_time, self.start_time), bbox_inches='tight')
                 ODfig.clf(); allODs = None; ODthr.figure = None; ODthr = None; ODfig = None; fig = None; allconcs= None; colors = None; DM = None
                 plt.close('all')
                 with open("/mnt/morbidodata/%s/%s/ODtemp_%s.png" % (self.sysstr, self.start_time, self.start_time), "rb") as file_content:
