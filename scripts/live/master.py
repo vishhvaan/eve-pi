9b6d7f4236c94985c07e25f5a9b6bd92a1a94398
vishhvaan <gopalav@lri-108141.ccf.org>
2019-08-04 11:15:26 -0400

    Added Real-time toggleable combined growth curves of multiplex experiments

--- scripts/live/master.py
+++ scripts/live/master.py
@@ -365,11 +365,11 @@ def slackresponder():
 
 
 def temp_sensor_func():
-    while True:
         base_dir = '/sys/bus/w1/devices/'
         device_folder = glob.glob(base_dir + '28*')[0]
         device_file = device_folder + '/w1_slave'
 
+    while True:
         f = open(device_file, 'r')
         lines = f.readlines()
         f.close()
